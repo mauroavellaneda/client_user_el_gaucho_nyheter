5805105d45a7a12a5825986c0c47026987630e80
SebastianN97 <sebbe132132@gmail.com>
2020-10-23 18:15:14 +0200

    modifying acceptance test, changing location param in url.

--- cypress/integration/userCanSeeArticlesBasedOnTheirLocation.feature.js
+++ cypress/integration/userCanSeeArticlesBasedOnTheirLocation.feature.js
@@ -11,7 +11,7 @@ describe("User can see news from current country", () => {
       });
       cy.route({
         method: "GET",
-        url: "http://localhost:3000/api/v1/articles/?local=sweden",
+        url: "http://localhost:3000/api/v1/articles/?location=Sweden",
         response: "fixture:local_news_sweden.json",
       });
       cy.visit("/", stubLocation({ latitude: 59.3501, longitude: 17.9658 }));
@@ -19,14 +19,30 @@ describe("User can see news from current country", () => {
 
     it("Visitor can see the articles in the Local News category", () => {
       cy.get("[data-cy='local-news']").click();
+      cy.get("[data-cy='current-location']").should(
+        "contain",
+        "Local news from: Sweden"
+      );
       cy.get("[data-cy='article-7']").within(() => {
         cy.contains("Osäkert kring Piteås match");
       });
     });
   });
 
-  xcontext("when current location is America", () => {
+  context("when current location is America", () => {
     beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles",
+        response: "fixture:articles_index.json",
+      });
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/v1/articles/?location=America",
+        response: "fixture:local_news_USA.json",
+      });
+
       cy.visit(
         "/",
         stubLocation({ latitude: 40.73061, longitude: -73.935242 })
