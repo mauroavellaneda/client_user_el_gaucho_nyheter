9845cf21d1389f7fc2c411f600c0b446b44575a8
mauro avellaneda <65618376+mauroavellaneda@users.noreply.github.com>
2020-11-16 12:29:10 +0100

    Revert "User can see articles based on their location"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,20 +10,18 @@ const Header = () => {
   return (
     <Menu inverted>
       <Menu.Item>
-        <h1 data-cy="header" id="">
-          El Gaucho Nyheter
-        </h1>
+        <h1 data-cy="header">El Gaucho Nyheter</h1>
         <Menu.Item as={Link} to="/" data-cy="home">
           Home
+        </Menu.Item>
+        <Menu.Item as={Link} to="/category/news" data-cy="news">
+          News
         </Menu.Item>
         <Menu.Item as={Link} to="/category/sports" data-cy="sports">
           Sports
         </Menu.Item>
         <Menu.Item as={Link} to="/category/politics" data-cy="politics">
           Politics
-        </Menu.Item>
-        <Menu.Item as={Link} to="/category/local" data-cy="local-news">
-          Local news
         </Menu.Item>
       </Menu.Item>
       {authenticated ? (
@@ -37,7 +35,7 @@ const Header = () => {
             Become Subscriber
           </Menu.Item>
         )
-      ) : (
+      ) : ( 
         <Menu.Item as={Link} to="/login" data-cy="login" position="right">
           Login
         </Menu.Item>
