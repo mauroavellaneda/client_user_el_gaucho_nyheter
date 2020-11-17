d3e1c67e4b6142b3db2f362558b85c17df2ef0a2
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-11-17 10:17:43 +0100

    Adds feature test

---
+++ cypress/integration/UserCanChooseUiLanguage.feature.js
@@ -0,0 +1,37 @@
+describe("Vistors can choose UI language", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/articles",
+      response: "fixture:articles_index.json",
+    });
+    cy.visit("/");
+  });
+  it("by choosing language to spanish", () => {
+    cy.get("[data-cy='change-language']").click();
+    cy.get("div[role='option']").contains("Spanish").click();
+    cy.get("[data-cy='sports']").should("contain", "Deportes");
+    cy.get("[data-cy='politics']").should("contain", "Politica");
+    cy.get("[data-cy='local-news']").should("contain", "Noticias Locales");
+    cy.get("[data-cy='login']").should("contain", "Inicia sesion");
+  });
+  it("by choosing language to swedish", () => {
+    cy.get("[data-cy='change-language']").click();
+    cy.get("div[role='option']").contains("Svenska").click();
+    cy.get("[data-cy='sports']").should("contain", "Sporter");
+    cy.get("[data-cy='home']").should("contain", "Hem");
+    cy.get("[data-cy='Politics']").should("contain", "Politik");
+    cy.get("[data-cy='news']").should("contain", "Nyheter");
+    cy.get("[data-cy='politics']").should("not.contain", "Politica");
+    cy.get("[data-cy='login']").should("contain", "Logga in");
+  });
+  it("by choosing language to swedish", () => {
+    cy.get("[data-cy='change-language']").click();
+    cy.get("div[role='option']").contains("Svenska").click();
+    cy.get("[data-cy='sports']").should("contain", "Sporter");
+    cy.get("[data-cy='politics']").should("not.contain", "Politica");
+    cy.get("[data-cy='local-news']").should("not.contain", "Noticias Locales");
+    cy.get("[data-cy='login']").should("contain", "Logga in");
+  });
+});
